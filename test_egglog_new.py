ea911f8d3ebc5aff347ae250f89e4acb491619bc
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-12-11 16:33:43 -0600

    LVA VSum working

--- test_egglog_new.py
+++ test_egglog_new.py
@@ -10,7 +10,10 @@ from egglog import (
     Expr,
     i64,
     i64Like,
+    Bool,
+    BoolLike,
     Vec,
+    Set,
     String,
     StringLike,
     EGraph,
@@ -52,7 +55,7 @@ class Value(Expr):
     def BoolFalse(cls) -> Value: ...
 
     @classmethod
-    def ConstI64(cls, val: i64) -> Value: ...
+    def ConstI64(cls, val: i64Like) -> Value: ...
 
     def toList(self) -> ValueList: ...
 
@@ -65,6 +68,17 @@ class ValueList(Expr):
     def append(self, vs: ValueList) -> ValueList: ...
 
     def toValue(self) -> Value: ...
+
+    def toSet(self) -> Set[Value]: ...
+
+    def map(self, fn: Callable[[Value], Value]) -> ValueList: ...
+
+    @classmethod
+    def Merge(cls,
+              merge_fn: Callable[[Value, Value], Value],
+              vas: ValueList,
+              vbs: ValueList) -> ValueList: ...
+
 
 
 class Term(Expr):
@@ -93,7 +107,7 @@ class TermList(Expr):
     def mapValue(self: TermList, fn: Callable[[Term], Value]) -> ValueList: ...
 
 
-def make_TermList(*args: Term) -> TermList:
+def termlist(*args: Term) -> TermList:
     return TermList(Vec(*args))
 
 
@@ -104,6 +118,8 @@ def Eval(env: Env, term: Term) -> Value: ...
 @function
 def EvalMap(env: Env, terms: TermList) -> ValueList: ...
 
+@function
+def VFix(v: Value) -> Value: ...
 
 @function
 def VGetPort(vs: ValueList, idx: i64) -> Value: ...
@@ -112,9 +128,25 @@ def VGetPort(vs: ValueList, idx: i64) -> Value: ...
 @function
 def VBranch(cond: Value, then: Value, orelse: Value) -> Value: ...
 
+@function(unextractable=True)
+def _LoopTemp(outs: ValueList) -> Value: ...
 
 @function
-def VLoop(body: Value) -> Value: ...
+def _LoopBack(phis: ValueList, body: Term) -> ValueList: ...
+
+@function
+def _LoopDropCond(outs: Value) -> ValueList: ...
+
+@function
+def _LoopOutputs(v: Value) -> ValueList: ...
+
+@function
+def _LoopPhiOf(phi: Value, loopback: Value) -> Value: ...
+@function
+def _MapLoopPhiOf(phis: ValueList, loopbacks: ValueList) -> ValueList: ...
+
+@function
+def VLoop(phis: ValueList, body: Value) -> Value: ...
 
 
 @function
@@ -130,12 +162,35 @@ def EnvEnter(env: Env, ins: TermList) -> Env: ...
 
 
 @function
-def EnvLoop(env: Env, loop: Term) -> Env: ...
+def EnvLoop(vs: ValueList) -> Env: ...
 
 
 @function
-def FoldBinOp(op: StringLike, lhs: Value, rhs: Value) -> Value: ...
+def PhiMap(left: ValueList, right: ValueList) -> ValueList: ...
 
+@function
+def VPhi(a: Value, b: Value) -> Value: ...
+
+
+class LVA(Expr):
+    """Loop Variable Analysis"""
+    ...
+
+@function
+def LVAnalysis(v: Value) -> LVA: ...
+
+@function
+def LoopIncremented(op: StringLike, phi: Value, init: Value, step: Value) -> LVA: ...
+@function
+def LoopIndVar(op: StringLike, start: Value, stop: Value, step: Value) -> LVA: ...
+@function
+def LoopAccumIndVar(op: StringLike, start: Value, stop: Value, step: Value) -> LVA: ...
+
+@function
+def IsLoopInVariant(v: Value) -> Bool: ...
+
+@function
+def VSum(vstart: Value, vstop: Value, vstep: Value) -> Value: ...
 
 @ruleset
 def _propagate_RegionDef_from_the_end(
@@ -176,65 +231,71 @@ def _VBranch(
     yield rewrite(VBranch(Value.BoolFalse(), va, vb)).to(vb)
 
 
-# @egraph.register
-# def _VLoop(
-#     env: Env,
-#     input_terms: Vec[Term],
-#     body_term: Term,
-#     vec_va: Vec[Value],
-#     vec_vb: Vec[Value],
-# ):
-#     # VLoop
-#     loop = Term.Loop(input_terms, body_term)
-#     yield rewrite(
-#         Eval(env, loop)
-#     ).to(
-#         VLoop(
-#             Eval(
-#                 EnvLoop(env, loop),
-#                 body_term,
-#             )
-#         ),
-#     )
-#     # EnvLoop
-#     yield rewrite(
-#         EnvLoop(env, loop),
-#     ).to(
-#         EnvEnter(env, VListFresh(input_terms)),
-#         # Env.nil().nest(
-#         #     VCons()
-#         #     VMergeList(
-#         #         EvalMap(env, input_terms),
-#         #         Unlist(DropListFirst(loop)),
-#         #     )
-#         # )
-#     )
-#     """
-#     a[0] = a0 | aN
-#     b[0] = b0 | bN
+@ruleset
+def _VLoop(
+    env: Env,
+    input_terms: TermList,
+    body_term: Term,
+    vec_va: Vec[Value],
+    vec_vb: Vec[Value],
+    vl: ValueList,
+    vl2: ValueList,
+    phis: ValueList,
+    va: Value,
+    vb: Value,
+):
+    # VLoop
+    yield rewrite(
+        Eval(env, Term.Loop(input_terms, body_term)),
+    ).to(
+        _LoopTemp(_LoopBack(EvalMap(env, input_terms), body_term))
+    )
 
-#     Eval(Env(a[0], b[0]), term)
+    yield rule(
+        eq(vl).to(_LoopBack(phis, body_term)),
+    ).then(
+        union(_LoopTemp(vl)).with_(
+            VLoop(_pm:=PhiMap(phis, vl),
+                  _inner:=Eval(EnvLoop(_pm), body_term))
+        ),
+        union(vl).with_(
+            _LoopDropCond(_inner)
+        )
+    )
 
+    yield rewrite(VLoop(vl, va)).to(_LoopOutputs(va).toValue())
 
-#     """
-#     # VMergeList
-#     yield rewrite(
-#         VMergeList(Value.List(vec_va), Value.List(vec_vb))
-#     ).to(
-#         VCons(vec_va[0] | vec_vb[0], VMergeList(Value.List(vec_va.remove(0)),
-#                                                 Value.List(vec_vb.remove(0)))),
-#         # given
-#         vec_va.length() > i64(0),
-#         vec_vb.length() > i64(0),
-#     )
-#     yield rewrite(
-#         VMergeList(Value.List(vec_va), Value.List(vec_vb))
-#     ).to(
-#         VConsEmpty(),
-#         # given
-#         eq(i64(0)).to(vec_va.length()),
-#         eq(i64(0)).to(vec_vb.length()),
-#     )
+    # EnvLoop
+    yield rewrite(
+        EnvLoop(phis)
+    ).to(
+        Env.nil().nest(phis)
+    )
+    # _LoopDropCond
+    yield rewrite(
+        _LoopDropCond(ValueList(vec_va).toValue())
+    ).to(
+        # Drop the loop condition
+        ValueList(vec_va.remove(0))
+    )
+
+    # _LoopOutputs
+    yield rewrite(
+        _LoopOutputs(ValueList(vec_va).toValue())
+    ).to(
+        # Drop the loop condition
+        ValueList(vec_va.remove(0)).map(VFix)
+    )
+
+    # PhiMap
+    yield rewrite(
+        PhiMap(vl, vl2)
+    ).to(
+        ValueList.Merge(VPhi, vl, vl2)
+    )
+
+    # Phi
+    yield rewrite(VPhi(va, vb)).to(va|vb)
 
 
 @ruleset
@@ -258,12 +319,32 @@ def _VGetPort(i: i64, vec_vals: Vec[Value], env: Env, term: Term):
         i < vec_vals.length(),
     )
 
+@ruleset
+def _Value_rules(a: Value, b: Value):
+    # __or__ associativity
+    yield rewrite( a | b ).to( b | a)
+
+    # merge phis
+    yield rewrite( a | a ).to( a )
+    # merge loop back phis
+    yield rule(
+        eq(a).to(a | b)
+    ).then(
+        union(b).with_(a),
+        set_(IsLoopInVariant(a)).to(Bool(True)),
+    )
+
+
+
 
 @ruleset
 def _ValueList_rules(
     vs1: Vec[Value],
     vs2: Vec[Value],
     vl: ValueList,
+    vl2: ValueList,
+    fn: Callable[[Value], Value],
+    merge_fn: Callable[[Value, Value], Value],
 ):
     yield rewrite(ValueList(vs1).append(ValueList(vs2))).to(
         ValueList(vs1.append(vs2))
@@ -277,29 +358,66 @@ def _ValueList_rules(
         # given
         eq(i64(1)).to(vs1.length()),
     )
+    # map
+    yield rewrite(ValueList(vs1).map(fn)).to(
+        valuelist(),
+        # given
+        eq(i64(0)).to(vs1.length()),
+    )
+    yield rewrite(ValueList(vs1).map(fn)).to(
+        valuelist(fn(vs1[0])).append(ValueList(vs1.remove(0)).map(fn)),
+        # given
+        vs1.length() > i64(0),
+    )
+    # Merge
+    yield rewrite(
+        ValueList.Merge(merge_fn, ValueList(vs1), ValueList(vs2))
+    ).to(
+        ValueList(Vec(merge_fn(vs1[0], vs2[0]))).append(
+            ValueList.Merge(merge_fn, ValueList(vs1.remove(0)), ValueList(vs2.remove(0)))
+        ),
+        # given
+        vs1.length() > i64(0),
+        vs2.length() > i64(0),
+    )
+    yield rewrite(
+        ValueList.Merge(merge_fn, ValueList(Vec[Value].empty()), ValueList(Vec[Value].empty()))
+    ).to(
+        ValueList(Vec[Value].empty())
+    )
+    # toSet
+    yield rewrite(
+        ValueList(vs1).toSet()
+    ).to(
+        Set(vs1[0]) | ValueList(vs1.remove(0)).toSet(),
+        # given
+        vs1.length() > i64(0),
+    )
+
+    yield rewrite(
+        ValueList(Vec[Value]()).toSet()
+    ).to(
+        Set[Value].empty()
+    )
 
 
 @ruleset
 def _EvalMap_to_ValueList(
     vec_terms: Vec[Term],
-    vec_vals: Vec[Value],
     env: Env,
-    val: Value,
-    term: Term,
     map_fn: Callable[[Term], Value],
-    term_list: TermList,
 ):
     # EvalMap to Value.List
     yield rewrite(EvalMap(env, TermList(vec_terms))).to(
         TermList(vec_terms).mapValue(partial(Eval, env)),
     )
     yield rewrite(TermList(vec_terms).mapValue(map_fn)).to(
-        new_values(),
+        valuelist(),
         # given
         eq(i64(0)).to(vec_terms.length()),
     )
     yield rewrite(TermList(vec_terms).mapValue(map_fn)).to(
-        new_values(map_fn(vec_terms[0])).append(
+        valuelist(map_fn(vec_terms[0])).append(
             TermList(vec_terms.remove(0)).mapValue(map_fn)
         ),
         # given
@@ -308,12 +426,15 @@ def _EvalMap_to_ValueList(
 
 
 @ruleset
-def _EnvEnter_EvalMap(vec_terms: Vec[Term], env: Env):
+def _EnvEnter_EvalMap(terms: TermList, env: Env):
     # EnvEnter
-    yield rewrite(EnvEnter(env, TermList(vec_terms))).to(
-        Env.nil().nest(EvalMap(env, TermList(vec_terms)))
+    yield rewrite(EnvEnter(env, terms)).to(
+        Env.nil().nest(EvalMap(env, terms))
     )
 
+@ruleset
+def _VBinOp_assoc(op: String, va: Value, vb: Value):
+    yield rewrite(VBinOp(op, va, vb)).to(VBinOp(op, vb, va))
 
 @ruleset
 def _VBinOp_Lt(env: Env, ta: Term, tb: Term, i: i64, j: i64):
@@ -341,27 +462,96 @@ def _VBinOp_Add(env: Env, ta: Term, tb: Term, i: i64, j: i64):
 
 
 @ruleset
-def _VBinOp_LoopFold(
+def _LoopAnalysis(
     env: Env,
     ta: Term,
     tb: Term,
     i: i64,
     j: i64,
     op: String,
+    op2: String,
     vres: Value,
     va: Value,
     vb: Value,
+    vc: Value,
+    vstart: Value,
+    vstop: Value,
+    vstep: Value,
+    vby: Value,
+    vcond: Value,
+    vn: Value,
+    vphis: Vec[Value],
+    phis: ValueList,
+    outs: ValueList,
+    vs: Vec[Value],
+    _a: Value,
+    _b: Value,
+    _c: Value,
+
+
 ):
-    yield rule(eq(vres).to(VBinOp(op, va, vb))).then(FoldBinOp(op, va, vb))
-    """
-    a = a0 | a1
+    # Match i += consti64 as LoopIncremented()
+    yield rule(
+        eq(vc).to(va | vb),
+        eq(va).to(VBinOp(op, vc, vby)),
+    ).then(
+        union(LVAnalysis(va)).with_(LoopIncremented(op, vc, vb, vby))
+    )
+    # Match LoopIncrement() < n as LoopIndVar if cond used by VLoop
+    yield rule(
+        VLoop(ValueList(vphis), ValueList(vs).toValue()),
+        eq(vcond).to(vs[0]),  # the condition
+        eq(vcond).to(VBinOp("Lt", vc, vn)),
+        vphis.contains(va),
+        eq(LVAnalysis(vc)).to(LoopIncremented(op, va, vb, vby)),
+        eq(i64(1) + vphis.length()).to(vs.length()), # wellformed
+    ).then(
+        union(LVAnalysis(va)).with_(LoopIndVar(op, vb, vn, vby))
+    )
+    # Match accumulator c += vstep
+    yield rule(
+        eq(LVAnalysis(va)).to(LoopIncremented(op, vb, vc, vby)),
+        eq(LVAnalysis(vby)).to(LoopIndVar(op2, vstart, vstop, vstep))
+    ).then(
+        union(LVAnalysis(vb)).with_(LoopAccumIndVar(op, vstart, vstop, vstep))
+    )
 
-    a1 = a + 1
+    # Help find the phi node
+    yield rule(
+        VLoop(ValueList(vphis), ValueList(vs).toValue()),
+        vs.length() > 1,
+    ).then(
+        _MapLoopPhiOf(ValueList(vphis), ValueList(vs.remove(0))),
+    )
+    yield rewrite(_MapLoopPhiOf(ValueList(vphis), ValueList(vs))).to(
+        valuelist(_LoopPhiOf(vphis[0], vs[0])).append(_MapLoopPhiOf(ValueList(vphis.remove(0)), ValueList(vs.remove(0)))),
+        # given
+        vphis.length() > 0
+    )
+    yield rewrite(_MapLoopPhiOf(valuelist(), valuelist())).to(valuelist())
 
-    """
+    # Match VFix(v) and LVAnalysis(v) is LoopAccumIndVar
+    yield rewrite(
+        VFix(va)
+    ).to(
+        VSum(vstart, vstop, vstep),
+        # given
+        _LoopPhiOf(vb, va),
+        eq(LVAnalysis(vb)).to(LoopAccumIndVar("Add", vstart, vstop, vstep))
+    )
 
 
-def new_values(*args: Value) -> ValueList:
+
+
+
+@ruleset
+def _Eval_Term_Literals(
+    env: Env,
+    i: i64,
+):
+    yield rewrite(Eval(env, Term.LiteralI64(i))).to(Value.ConstI64(i))
+
+def valuelist(*args: Value) -> ValueList:
     if not args:
         return ValueList(Vec[Value]())
     return ValueList(Vec(*args))
@@ -369,15 +559,20 @@ def new_values(*args: Value) -> ValueList:
 
 def make_rules():
     return (
-        _propagate_RegionDef_from_the_end
+        _Value_rules
+        | _Eval_Term_Literals
+        | _propagate_RegionDef_from_the_end
         | _VBranch
+        | _VLoop
         | _eval_ins_get
         | _VGetPort
         | _EvalMap_to_ValueList
         | _ValueList_rules
         | _EnvEnter_EvalMap
+        | _VBinOp_assoc
         | _VBinOp_Lt
         | _VBinOp_Add
+        | _LoopAnalysis
     )
 
 
@@ -420,38 +615,34 @@ def region_builder(nin: int):
         ins = region.begin()
         outs = fn(region, ins)
         assert isinstance(outs, (tuple, list))
-        return region.end(make_TermList(*outs))
+        return region.end(termlist(*outs))
 
     return wrapped
 
 
 def saturate(egraph: EGraph, schedule):
     if DEBUG:
-        egraph.saturate(schedule, n_inline_leaves=2)
+        egraph.saturate(schedule, n_inline_leaves=1)
     else:
         egraph.run(schedule)
 
 
 def test_straight_line_basic():
-    from egglog import eq
-
     @region_builder(5)
     def main(region, ins):
         return list(map(ins.get, range(5)))
 
     env = Env.nil()
-    env = env.nest(new_values(*map(Value.Param, range(5))))
+    env = env.nest(valuelist(*map(Value.Param, range(5))))
     root = Eval(env, main)
 
     checks = [
-        eq(root).to(new_values(*map(Value.Param, range(5))).toValue()),
+        eq(root).to(valuelist(*map(Value.Param, range(5))).toValue()),
     ]
     run(root, checks=checks)
 
 
 def test_max_if_else():
-    from egglog import eq
-
     fn = RegionDef("main", 2)
 
     @region_builder(2)
@@ -477,14 +668,14 @@ def test_max_if_else():
             return [a]
 
         # Do Branch
-        ifthen = Term.Branch(lt, make_TermList(a, b), if_then, or_else)
+        ifthen = Term.Branch(lt, termlist(a, b), if_then, or_else)
 
         # Return
         return [ifthen.getPort(0)]
 
     # Eval with Env
     env = Env.nil()
-    env = env.nest(new_values(Value.ConstI64(0), Value.ConstI64(1)))
+    env = env.nest(valuelist(Value.ConstI64(0), Value.ConstI64(1)))
     root = Eval(env, main)
 
     checks = [
@@ -493,49 +684,42 @@ def test_max_if_else():
     run(root, checks=checks)
 
 
-# def test_sum_loop():
-#     from egglog import eq
+def test_sum_loop():
+    @region_builder(2)
+    def main(region, ins):
+        init = ins.get(0)
+        n = ins.get(1)
+        c = init
+        i = Term.LiteralI64(0)
 
-#     fn = RegionDef("main", 2)
+        @region_builder(2)
+        def body(region, ins):
+            i = ins.get(0)
+            n = ins.get(1)
+            c = ins.get(2)
 
-#     @region_builder(2)
-#     def main(region, ins):
-#         ctx_body = fn.begin()
-#         init = ctx_body.get(0)
-#         n = ctx_body.get(1)
-#         c = init
-#         i = Term.LiteralI64(0)
+            c = Term.Add(c, i)
+            i = Term.Add(i, Term.LiteralI64(1))
+            lt = Term.Lt(i, n)
+            return [lt, i, n, c]
 
-#         @region_builder(2)
-#         def body(region, ins):
-#             i = ins.get(0)
-#             n = ins.get(1)
-#             c = ins.get(2)
+        # Do Loop
+        loop = Term.Loop(termlist(i, n, c), body)
 
-#             c = Term.Add(c, i)
-#             i = Term.Add(i, Term.LiteralI64(1))
-#             lt = Term.Lt(i, n)
-#             return [lt, i, n, c]
+        # Return
+        return [loop.getPort(2)]
 
-#         # Do Loop
-#         loop = Term.Loop(Vec(i, n, c), body)
+    # Eval with Env
+    env = Env.nil()
+    env = env.nest(valuelist(Value.ConstI64(0), Value.ConstI64(10)))
+    root = Eval(env, main)
 
-#         # Return
-#         return [loop.getPort(2)]
-
-#     # Eval with Env
-#     env = Env.nil()
-#     env = env.nest(Value.List(Vec(Value.ConstI64(0), Value.ConstI64(10))))
-#     root = Eval(env, main)
-
-#     checks = [
-#         eq(root).to(Value.List(Vec(Value.ConstI64(1)))),
-#     ]
-#     run(root, checks=checks)
+    checks = [
+        # eq(root).to(valuelist(Value.ConstI64(1)).toValue()),
+    ]
+    run(root, checks=checks)
 
 
 if __name__ == "__main__":
-    test_max_if_else()
+    test_sum_loop()
 
-
-# test_straight_line_basic()
