ea11136a32453a0767e66b97b7ade47bae65f3ea
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2025-02-21 13:56:01 -0600

    Remove old tests

--- sealir/tests/test_rvsdg.py
+++ sealir/tests/test_rvsdg.py
@@ -270,6 +270,42 @@ def test_f_o_r_t_r_a_n():
     run(foo, args, localscope=ChainMap(locals(), globals()))
 
 
+def test_if_else():
+
+    def udt(c):
+        a = c - 1
+        if a < c:
+            b = a + 2
+        else:
+            pass
+        return b + 3
+
+    rvsdg_ir, dbginfo = rvsdg.restructure_source(udt)
+    print(dbginfo.show_sources())
+    args = (10,)
+    kwargs = {}
+    res = evaluate(rvsdg_ir, args, kwargs, dbginfo=dbginfo)
+    print("res =", res)
+    assert res == udt(*args, **kwargs)
+
+
+def test_for_loop():
+
+    def udt(n):
+        c = 0
+        for i in range(n):
+            c += i
+        return c
+
+    rvsdg_ir, dbginfo = rvsdg.restructure_source(udt)
+    print(dbginfo.show_sources())
+    args = (10,)
+    kwargs = {}
+    res = evaluate(rvsdg_ir, args, kwargs, dbginfo=dbginfo)
+    print("res =", res)
+    assert res == udt(*args, **kwargs)
+
+
 def run(func, args, *, localscope=None):
     # Get expected result
     expected = func(*args)
